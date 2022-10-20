3d8c38394a016af5eedb701ce1d578b08379ec5b
Kensuke Fukami <kensuke8293@gmail.com>
2022-10-20 16:04:34 +0900

    fix : エラー修正

--- cruds/prameta.py
+++ cruds/prameta.py
@@ -24,7 +24,29 @@ def my_get_parameta():
     return pokemon_data
 
 
-def pokemon_parameta(pokemon_data, endevor: list):
+def my_pokemon_regsist(pokemondata, endever):
+    my_pokemon_data = pokemon_parameta(pokemondata, endever)
+    conn = sqlite3.connect(dbname)
+    c = conn.cursor()
+    sql = "insert into My_pokemon_parameta(name,main_type,sub_type,hp,attack,deffence,sp_attack,sp_deffence,speed)values(?,?,?,?,?,?,?,?,?)"
+    data = (
+        my_pokemon_data[0],
+        my_pokemon_data[1],
+        my_pokemon_data[2],
+        int(my_pokemon_data[3]),
+        int(my_pokemon_data[4]),
+        int(my_pokemon_data[5]),
+        int(my_pokemon_data[6]),
+        int(my_pokemon_data[7]),
+        int(my_pokemon_data[8]),
+    )
+    c.execute(sql, data)
+    conn.commit()
+    conn.close()
+    return 0
+
+
+def pokemon_set_parameta(pokemon_data, endevor: list):
     pokemon_para_data = []
     _endevor = endevor[0].split(",")
     for i in range(0, 9):
