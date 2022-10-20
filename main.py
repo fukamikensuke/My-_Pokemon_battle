6b4a211009480cd7a911aac4db8a4f30113e4abd
Kensuke Fukami <kensuke8293@gmail.com>
2022-10-20 13:56:10 +0900

    feat:努力値のdb書き込みの実装

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from pickletools import int4
 from threading import currentThread
 from tkinter import INSERT
 import pandas as pd
 import sqlite3
 from fastapi import FastAPI, Form
 import cruds.party as party
+import cruds.prameta as parameta
+import array
 
 app = FastAPI()
 
@@ -15,8 +18,12 @@ dbname = "pokemon.db"
 conn = sqlite3.connect(dbname)
 cur = conn.cursor()
 cur.execute("DROP TABLE IF EXISTS My_pokemon")
+cur.execute("DROP TABLE IF EXISTS My_pokemon_parameta")
 cur.execute(
     "CREATE TABLE My_pokemon(id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING)"
+)
+cur.execute(
+    "CREATE TABLE My_pokemon_parameta(name STRING,main_type STRING,sub_type STRING,hp INTEGER,attack INTEGER,deffence INTEGER,sp_attack INTEGER,sp_deffence INTEGER,speed INTEGER)"
 )
 conn.close()
 
@@ -46,20 +53,36 @@ async def regist(
     return {"message": "Ok"}  # ユーザー名が返ってくる
 
 
-@app.get("/my_regist/parameta")
-def my_prameta():
+@app.post("/my_regist/parameta")
+def my_prameta(
+    p1: list = Form(),
+    p2: list = Form(),
+    p3: list = Form(),
+    p4: list = Form(),
+    p5: list = Form(),
+    p6: list = Form(),
+):
+    pokemon_data = parameta.my_get_parameta()
+    my_pokemon_data = parameta.pokemon_parameta(pokemon_data[0], p1)
+    print(pokemon_data[0])
     conn = sqlite3.connect(dbname)
-    cur = conn.cursor()
-    cur.execute("SELECT name FROM My_pokemon")
-    for row in cur:
-        print(row[0])
-        my_pokemon.append(row[0])
-    conn.close
-    pokemon_data = []
-    for i in my_pokemon:
-        pokemon_data.append((df[df["名前(フォルム)"] == i]).values[0])
-    print(pokemon_data)
-    return pokemon_data
+    c = conn.cursor()
+    sql = "insert into My_pokemon_parameta(name,main_type,sub_type,hp,attack,deffence,sp_attack,sp_deffence,speed)values(?,?,?,?,?,?,?,?,?)"
+    data = (
+        my_pokemon_data[0],
+        my_pokemon_data[1],
+        my_pokemon_data[2],
+        int(my_pokemon_data[3]),
+        int(my_pokemon_data[4]),
+        int(my_pokemon_data[5]),
+        int(my_pokemon_data[6]),
+        int(my_pokemon_data[7]),
+        int(my_pokemon_data[8]),
+    )
+    c.execute(sql, data)
+    conn.commit()
+    conn.close()
+    return {"message": "Ok"}
 
 
 # for i in input_name_list:
